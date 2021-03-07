248f1f65f93140d7af57f21fd3a1b715d4ffcef3
DhyutiGopal <41200073+DhyutiGopal@users.noreply.github.com>
2021-03-07 18:16:20 -0500

    Final Project

---
+++ main.js
@@ -0,0 +1,161 @@
+
+
+
+/////////////////////////////////////////////////////////////////////////////
+/////////////////////////MAIN GAME////////////////////////////////////////////////////
+
+const problemElement = document.querySelector(".problem")
+const ourForm = document.querySelector(".our-form")
+const ourField = document.querySelector(".our-field")
+const pointsNeeded = document.querySelector(".points-needed")
+const mistakesAllowed = document.querySelector(".mistakes-allowed")
+const progressBar = document.querySelector(".progress-inner")
+const endMessage = document.querySelector(".end-message")
+const resetButton = document.querySelector(".reset-button")
+
+let state = {
+  score: 0,
+  wrongAnswers: 0
+}
+
+function updateProblem() {
+  state.currentProblem = generateProblem()
+  problemElement.innerHTML = `${state.currentProblem.numberOne} ${state.currentProblem.operator} ${state.currentProblem.numberTwo}`
+  ourField.value = ""
+  ourField.focus()
+}
+
+updateProblem()
+
+function generateNumber(max) {
+  return Math.floor(Math.random() * (max + 1))
+}
+
+function generateProblem() {
+  return {
+    numberOne: generateNumber(10),
+    numberTwo: generateNumber(10),
+    operator: ['+', '-', 'x'][generateNumber(2)]
+  }
+}
+
+ourForm.addEventListener("submit", handleSubmit)
+
+function handleSubmit(e) {
+  e.preventDefault()
+
+  let correctAnswer
+  const p = state.currentProblem
+  if (p.operator == "+") correctAnswer = p.numberOne + p.numberTwo
+  if (p.operator == "-") correctAnswer = p.numberOne - p.numberTwo
+  if (p.operator == "x") correctAnswer = p.numberOne * p.numberTwo
+
+  if (parseInt(ourField.value, 10) === correctAnswer) {
+    state.score++
+    pointsNeeded.textContent = 10 - state.score
+    updateProblem()
+    renderProgressBar()
+  } else {
+    state.wrongAnswers++
+    mistakesAllowed.textContent = 2 - state.wrongAnswers
+    problemElement.classList.add("animate-wrong")
+    setTimeout(() => problemElement.classList.remove("animate-wrong"), 451)
+  }
+  checkLogic()
+}
+
+function checkLogic() {
+  // if you won
+  if (state.score === 10) {
+    endMessage.textContent = "Congrats! You won."
+    document.body.classList.add("overlay-is-open")
+    setTimeout(() => resetButton.focus(), 331)
+  }
+
+  // if you lost
+  if (state.wrongAnswers === 3) {
+    endMessage.textContent = "Sorry! You lost."
+    document.body.classList.add("overlay-is-open")
+    setTimeout(() => resetButton.focus(), 331)
+  }
+}
+
+resetButton.addEventListener("click", resetGame)
+
+function resetGame() {
+  document.body.classList.remove("overlay-is-open")
+  updateProblem()
+  state.score = 0
+  state.wrongAnswers = 0
+  pointsNeeded.textContent = 10
+  mistakesAllowed.textContent = 2
+  renderProgressBar()
+}
+
+function renderProgressBar() {
+  progressBar.style.transform = `scaleX(${state.score / 10})`
+}
+
+
+
+///////////////////////////////////////////////////////////////////////////
+
+//////////////////////////////////////////////////////////////
+/////////////////////STOP WATCH/////////////////////////////////////////
+
+const timer = document.getElementById('stopwatch');
+
+var hr = 0;
+var min = 0;
+var sec = 0;
+var stoptime = true;
+
+function startTimer() {
+  if (stoptime == true) {
+        stoptime = false;
+        timerCycle();
+    }
+}
+function stopTimer() {
+  if (stoptime == false) {
+    stoptime = true;
+  }
+}
+
+function timerCycle() {
+    if (stoptime == false) {
+    sec = parseInt(sec);
+    min = parseInt(min);
+    hr = parseInt(hr);
+
+    sec = sec + 1;
+
+    if (sec == 60) {
+      min = min + 1;
+      sec = 0;
+    }
+    if (min == 60) {
+      hr = hr + 1;
+      min = 0;
+      sec = 0;
+    }
+
+    if (sec < 10 || sec == 0) {
+      sec = '0' + sec;
+    }
+    if (min < 10 || min == 0) {
+      min = '0' + min;
+    }
+    if (hr < 10 || hr == 0) {
+      hr = '0' + hr;
+    }
+
+    timer.innerHTML = hr + ':' + min + ':' + sec;
+
+    setTimeout("timerCycle()", 1000);
+  }
+}
+
+function resetTimer() {
+    timer.innerHTML = '00:00:00';
+}
